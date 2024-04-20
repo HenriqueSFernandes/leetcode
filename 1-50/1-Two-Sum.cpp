657aae12f1d02254be799bc0f9070afef3df288b
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2024-04-20 20:19:55 +0100

    Added explanation to 1

--- 1-50/1-Two-Sum.cpp
+++ 1-50/1-Two-Sum.cpp
@@ -5,6 +5,14 @@ using namespace std;
 
 class Solution {
    public:
+   /**
+    * @brief Given an array of numbers, find the indices of the 2 numbers that add up to a target value.
+    * This solution iterates over the array, for each number it saves its complement (target - number) to a map. 
+    * If the number is already in the map, the solution has been found.
+    * @param nums The array.
+    * @param target The target.
+    * @return vector<int> 
+    */
     vector<int> twoSum(vector<int> &nums, int target) {
         unordered_map<int, int> map;
         vector<int> result;
